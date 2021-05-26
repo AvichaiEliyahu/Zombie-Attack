0eb3536a34f14ee5028c385429e0368c10632ed2
AvichaiEliyahu <avihayboom@gmail.com>
2021-05-26 10:24:19 +0300

    added start menu

--- Zombie Attack/Assets/Scripts/SceneLoader.cs
+++ Zombie Attack/Assets/Scripts/SceneLoader.cs
@@ -5,6 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] Canvas startGameCanvas;
+
+    private void Start()
+    {
+        startGameCanvas.enabled = true;
+        Time.timeScale = 0;
+        FindObjectOfType<WeaponSwitcher>().enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void StartGame()
+    {
+
+        startGameCanvas.enabled = false;
+        FindObjectOfType<WeaponSwitcher>().enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1;
+    }
     public void ReloadGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
